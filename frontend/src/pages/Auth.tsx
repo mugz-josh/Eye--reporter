70cbbfbbe6b49b51a44d45a84dccb104bded8b29
mugishajoshua <joshua.mugisha.upti@gmail.com>
2025-12-10 03:27:25 -0800

    solved error

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -266,7 +266,7 @@ export default function Auth() {
                   <Button
                     type="submit"
                     className="btn-full"
-   s                 disabled={signupLoading}
+                  disabled={signupLoading}
                   >
                     {signupLoading ? "Creating Account..." : "SIGN UP"}
                   </Button>
